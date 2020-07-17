7dc87a2dcad6ff1926fa60b9806c46754318993a
Craig Chasseur <spoonboy42@gmail.com>
2020-07-16 22:53:35 -0700

    Improve tooltip styling

--- web/js/main.js
+++ web/js/main.js
@@ -1,9 +1,13 @@
 google.charts.load('current', {'packages':['timeline']});
 google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
-  var dataRows = [];
-
+  var tooltipStyle = "font-family: sans-serif; " +
+                     "font-size: medium; " +
+                     "max-width: 50ch; " +
+                     "padding: 0ch 2ch;"
   var globalEndDate = new Date(2023, 0, 1);
+
+  var dataRows = [];
   for (var i = 0; i < consoles.length; i++) {
     var thisConsole = consoles[i];
 
@@ -14,8 +18,11 @@ function drawChart() {
       endDate.setMonth(endDate.getMonth() - 2);
     }
 
-    var tooltip = "<b>CPU: " + thisConsole.cpu + "</b>" +
-                  "<br><p>" + thisConsole.description + "</p>";
+    var tooltip = "<div style=\"" + tooltipStyle + "\">" +
+                  "<p style=\"font-weight: bold\">CPU: " +
+                      thisConsole.cpu + "</p>" +
+                  "<p>" + thisConsole.description + "</p>" +
+                  "</div>";
     var style = "color: " + arch_info[thisConsole.arch].color + ";"
 
     dataRows.push([thisConsole.manufacturer, thisConsole.name, tooltip, style,
@@ -28,7 +35,7 @@ function drawChart() {
 
   dataTable.addColumn({ type: 'string', id: 'Manufacturer' });
   dataTable.addColumn({ type: 'string', id: 'Console' });
-  dataTable.addColumn({ type: 'string', role: 'tooltip' });
+  dataTable.addColumn({ type: 'string', role: 'tooltip', p: { html: true} });
   dataTable.addColumn({ type: 'string', role: 'style' });
   dataTable.addColumn({ type: 'date', id: 'Start' });
   dataTable.addColumn({ type: 'date', id: 'End' });
@@ -38,7 +45,8 @@ function drawChart() {
     timeline: {
       rowLabelStyle: { fontSize: 16 },
       barLabelStyle: { fontSize: 16 }
-    }
+    },
+    tooltip: { isHtml: true }
   };
   chart.draw(dataTable, options);
 }
